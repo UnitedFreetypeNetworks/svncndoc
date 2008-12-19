17c429d8d3d1795fcaf7f527c356263a10a6e362
daijun <daijun@gmail.com>
2008-12-19 08:15:14 +0000

    some fix

---
+++ other/Subversion.mm
@@ -0,0 +1,20 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1229276318796" ID="Freemind_Link_1901611349" MODIFIED="1229276325375" TEXT="Subversion">
+<node CREATED="1229276333750" ID="_" MODIFIED="1229276341265" POSITION="right" TEXT="&#x670d;&#x52a1;&#x5668;">
+<node CREATED="1229276417593" ID="Freemind_Link_1477278842" MODIFIED="1229276437500" TEXT="&#x5b98;&#x65b9;&#x7a0b;&#x5e8f;"/>
+<node CREATED="1229276519812" ID="Freemind_Link_905953839" MODIFIED="1229276535046" TEXT="Collabnet Server"/>
+<node CREATED="1229276539718" ID="Freemind_Link_815477327" MODIFIED="1229276805984" TEXT="VisualSVN Server&#xff08;Windows&#xff09;"/>
+</node>
+<node CREATED="1229276344312" ID="Freemind_Link_1481608263" MODIFIED="1229276385796" POSITION="left" TEXT="&#x6587;&#x6863;"/>
+<node CREATED="1229276361875" ID="Freemind_Link_336746204" MODIFIED="1229276367062" POSITION="right" TEXT="&#x5ba2;&#x6237;&#x7aef;">
+<node CREATED="1229276585546" ID="Freemind_Link_1043804433" MODIFIED="1229276634312" TEXT="&#x5b98;&#x65b9;&#x547d;&#x4ee4;&#x884c;&#xff08;&#x8de8;&#x5e73;&#x53f0;&#xff09;"/>
+<node CREATED="1229276636078" ID="Freemind_Link_1978737865" MODIFIED="1229276655875" TEXT="TortoiseSVN&#xff08;Windows&#xff09;"/>
+</node>
+<node CREATED="1229276391828" ID="Freemind_Link_1644695983" MODIFIED="1229276394718" POSITION="left" TEXT="&#x6559;&#x7a0b;"/>
+<node CREATED="1229276669531" ID="Freemind_Link_811065487" MODIFIED="1229276699562" POSITION="right" TEXT="IDE&#x96c6;&#x6210;">
+<node CREATED="1229276700734" ID="Freemind_Link_594748609" MODIFIED="1229276731515" TEXT="AnhkSVN&#xff08;Visual Studio&#xff09;"/>
+<node CREATED="1229276733687" ID="Freemind_Link_1808892180" MODIFIED="1229276752375" TEXT="Subclipse&#xff08;Eclipse&#xff09;"/>
+</node>
+</node>
+</map>
